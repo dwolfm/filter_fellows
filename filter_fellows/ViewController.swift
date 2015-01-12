4f496c7fcb87f830e2d3c3bce2dc8a6f41113c95
Duncan Marsh <dwolfm@gmail.com>
2015-01-12 13:10:42 -0800

    Initial Commit

---
+++ filter_fellows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  filter_fellows
+//
+//  Created by nacnud on 1/12/15.
+//  Copyright (c) 2015 nacnud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
